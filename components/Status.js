64259872e7ac77100006ab57623c35b774dd2a50
PaulLandaeta <paulwilkerlf@gmail.com>
2019-05-16 19:29:43 -0400

    add styles status bar

--- components/Status.js
+++ components/Status.js
@@ -1,11 +1,35 @@
 import { Constants } from "expo";
-import { StyleSheet } from "react-native";
+import {
+  NetInfo,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
+import React from "react";
 
 const statusHeight = Platform.OS === "ios" ? Constants.statusBarHeight : 0;
+
+export default class Status extends React.Component {
+  state = {
+    isConnected: null
+  };
+  // ...
+  render() {
+    const { isConnected } = this.state;
+    const backgroundColor = isConnected ? "white" : "red";
+    if (Platform.OS === "ios") {
+      return <View style={[styles.status, { backgroundColor }]} />;
+    }
+    return null; // Temporary!
+  }
+}
+
 const styles = StyleSheet.create({
   status: {
     zIndex: 1,
     height: statusHeight
   }
-  // ...
+  // /*  */...
 });
